ff5432f794cd9ab5001c0efcb693f5d00222e22c
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-04-04 21:36:30 +0200

    fix(dev/backend): removed useless var

--- aws/dev/backend/variables.tf
+++ aws/dev/backend/variables.tf
@@ -3,11 +3,6 @@ variable "aws_profile" {
   default = "891377085136_Admin"
 }
 
-variable "env" {
-  type    = string
-  default = "dev"
-}
-
 variable "domain_name" {
   type    = string
   default = "dev.hearch.co"
