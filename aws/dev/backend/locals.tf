dda82c25d926d843ce8a2dc5eb508e7ee342b85d
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-04-07 16:38:11 +0200

    fix(lambda): CORS

--- aws/dev/backend/locals.tf
+++ aws/dev/backend/locals.tf
@@ -1,7 +1,7 @@
 locals {
   # Lambda
   environment = tomap({
-    HEARCHCO_SERVER_FRONTENDURLS = "http://localhost:5173,https://*hearch.co,https://*hearchco-frontend.pages.dev"
+    HEARCHCO_SERVER_FRONTENDURLS = "http://localhost:5173,https://hearch.co,https://frontend-*-hearchco.vercel.app"
     HEARCHCO_SERVER_CACHE_TYPE   = "none" # currently caching has some bugs so it's disabled
   })
   # Not inside environment because it's a secret
