71eee866865f592d6a5316bde70c66278791e9bc
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-04-11 11:26:29 +0200

    feat(prod): added all supported EU and US regions

---
+++ aws/prod/backend/region_eu_west_1.tf
@@ -0,0 +1,55 @@
+# To create a new region copy this file and rename it to the region name.
+# Afterwards use Find and Replace to replace all occurrences "eu-west-1" with the new region name.
+# Lastly, use Find and Replace to replace all occurrences "eu_west_1" with the new region name.
+
+provider "aws" {
+  profile = var.aws_profile
+  region  = "eu-west-1"
+  alias   = "eu-west-1"
+}
+
+module "hearchco_s3_eu_west_1" {
+  source = "../../modules/backend/s3"
+
+  providers = {
+    aws = aws.eu-west-1
+  }
+}
+
+module "hearchco_lambda_eu_west_1" {
+  source           = "../../modules/backend/lambda"
+  role             = module.lambda_iam.role_arn
+  s3_bucket        = module.hearchco_s3_eu_west_1.bucket_id
+  s3_key           = module.hearchco_s3_eu_west_1.s3_key
+  source_code_hash = module.hearchco_s3_eu_west_1.source_code_hash
+  environment      = local.environment
+  proxy_salt       = local.proxy_salt
+
+  providers = {
+    aws = aws.eu-west-1
+  }
+}
+
+module "hearchco_certificate_eu_west_1" {
+  source         = "../../modules/backend/acm"
+  domain_name    = local.api_gateway_domain_name
+  hosted_zone_id = data.aws_route53_zone.hearchco_route53.zone_id
+
+  providers = {
+    aws = aws.eu-west-1
+  }
+}
+
+module "hearchco_apigateway_eu_west_1" {
+  source              = "../../modules/backend/apigateway"
+  domain_name         = local.api_gateway_domain_name
+  hosted_zone_id      = data.aws_route53_zone.hearchco_route53.zone_id
+  acm_certificate_arn = module.hearchco_certificate_eu_west_1.cert_arn
+  function_name       = module.hearchco_lambda_eu_west_1.function_name
+  invoke_arn          = module.hearchco_lambda_eu_west_1.invoke_arn
+  routes              = local.routes
+
+  providers = {
+    aws = aws.eu-west-1
+  }
+}
