c8c38410e2269f2610ebaa29da55155178b17d8e
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-08-12 14:06:26 +0200

    fix(dynamodb): policy data generation

--- modules/dynamodb/policy.tf
+++ modules/dynamodb/policy.tf
@@ -1,22 +1,24 @@
+data "aws_iam_policy_document" "dynamodb_policy_document" {
+  count = var.lambda_iam_role ? 1 : 0
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:PutItem",
+      "dynamodb:GetItem",
+      "dynamodb:UpdateItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:Query",
+      "dynamodb:Scan"
+    ]
+    resources = ["arn:aws:dynamodb:*:*:table/${aws_dynamodb_table.table.name}"]
+  }
+}
+
 resource "aws_iam_policy" "dynamodb_policy" {
   count = var.lambda_iam_role ? 1 : 0
 
   name        = "${replace(var.name, "-", "_")}_dynamodb_access_policy"
   description = "Policy to allow Lambda to access DynamoDB"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "dynamodb:PutItem",
-        "dynamodb:GetItem",
-        "dynamodb:UpdateItem",
-        "dynamodb:DeleteItem",
-        "dynamodb:Query",
-        "dynamodb:Scan"
-      ]
-      Resource = "arn:aws:dynamodb:*:*:table/${aws_dynamodb_table.table.name}"
-    }]
-  })
+  policy      = data.aws_iam_policy_document.dynamodb_policy_document[0].json
 }
